e81e2105a2b2913e6b1ae42eaf75c2573d6e1964
sierrahotel777 <sierrahotel777@gmail.com>
2025-01-21 22:40:45 +0530

    Completed Exercises

--- routes/user/Auth.js
+++ routes/user/Auth.js
@@ -3,9 +3,9 @@ const UserController = require('../../controller/user/UserController');
 const router = express.Router();
 
 router.post('/InputRecord', UserController.InputRecord);
-router.get('/students', UserController.GetRecord);
-router.get('/students/:id', UserController.GetRecordById);
-router.patch('/students/:id', UserController.UpdateRecordById);
-router.delete('/students/:id', UserController.DeleteRecord);
+router.get('/GetRecord', UserController.GetRecord);
+router.get('/GetRecord/:id', UserController.GetRecordById);
+router.patch('/UpdateRecord/:id', UserController.UpdateRecordById);
+router.delete('/DeleteRecord/:id', UserController.DeleteRecord);
 
 module.exports = router;
